99f154734dcdbde953de25ba02ab18a67b9ce7aa
Илья <ilacikoridze357@gmail.com>
2022-12-23 13:54:55 +0300

    Числа > 0

---
+++ test1/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.Clear();
+int inputNumber(string str)
+{
+    int number;
+    string text;
+    while (true)
+    {
+        System.Console.Write(str);
+        text = Console.ReadLine();
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        System.Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+    }
+    return number;
+}
+int myArr = inputNumber("Введите сколько чисел будет проверяться: ");
+int[] arr = new int[myArr];
+int res = 0;
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = inputNumber("\nВведите число : ");
+
+}
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        res++;
+    }
+}
+System.Console.WriteLine();
+Console.WriteLine(string.Join(" | ", arr));
+System.Console.WriteLine($"Чисел > 0 = {res}");
